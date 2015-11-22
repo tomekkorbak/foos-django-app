d985dbccf935c2ec09273adb3a9d6809bc997371
Tomek Korbak <tomasz.korbak@gmail.com>
2015-11-23 00:40:04 +0100

    Fix mMigration and PEP

--- foo_app/migrations/0001_initial.py
+++ foo_app/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=200)),
-                ('slug', models.CharField(max_length=200)),
+                ('slug', models.SlugField(max_length=200)),
                 ('text', models.TextField()),
             ],
         ),
--- foo_app/tests.py
+++ foo_app/tests.py
@@ -14,31 +14,30 @@ class IndexViewTest(TestCase):
         self.assertQuerysetEqual(response.context['foo_list'], [])
 
     def test_index_with_one_foo(self):
-        example_foo = Foo.objects.create(name='Example foo',
+        Foo.objects.create(name='Example foo',
                                          text='Lorem ipsum')
         response = self.client.get(reverse('index'))
         self.assertQuerysetEqual(response.context['foo_list'],
                                  ['<Foo: Example foo>'])
 
     def test_index_with_several_foos(self):
-        n = 10
         foos = [Foo.objects.create(name='Example foo %s' % number,
                                    text='Lorem ipsum')
-                for number in xrange(n)]
+                for number in xrange(10)]
         response = self.client.get(reverse('index'))
-        self.assertEqual(len(response.context['foo_list']), n)
+        self.assertEqual(len(response.context['foo_list']), len(foos))
 
 
 class DetailViewTest(TestCase):
 
     def test_detail_view_for_existing_foo(self):
         Foo.objects.create(name='Example foo',
-                                         text='Lorem ipsum',)
+                           text='Lorem ipsum',)
         response = self.client.get(
             reverse('detail', args=[slugify('Example foo')])
         )
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Lorem ipsum")
+        self.assertContains(response, 'Lorem ipsum')
 
     def test_detail_view_for_non_existent_foo(self):
         response = self.client.get(
@@ -64,6 +63,6 @@ class APIViewTest(TestCase):
             loaded_json = loads(response.content)
             self.assertIsInstance(loaded_json, list)
             self.assertIsInstance(loaded_json[0], dict)
-            self.assertEqual(len(loaded_json), 10)
+            self.assertEqual(len(loaded_json), len(foos))
         except ValueError:
             self.fail('No JSON object could be decoded')
